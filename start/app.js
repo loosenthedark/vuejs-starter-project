d5ca42164c253a42d1e41270b4a1159ed13a9e4e
paul Harrington <paulharrington@pauls-MacBook-Pro-4.local>
2021-06-14 13:19:24 +0100

    Add basic template functionality

--- start/app.js
+++ start/app.js
@@ -1,5 +1,5 @@
 // 1. Create a new Vue instance with the necessary options: el, data and methods. 
-    //Attach the new Vue instance to the div with the ID of "app". 
+//Attach the new Vue instance to the div with the ID of "app". 
 
 // 2. Create the following properties on the data object: title, summary, thumbnail, alt, and votes. 
 //   Make up data for the "title" and "summary" properties. 
@@ -13,10 +13,34 @@
 
 // 5. On the Vue instance, create a method called "increment" that adds 1 vote to the "votes" property 
 
-// 6. On the Vue instance, create a method called "decrement" that substracts 1 vote from the "votes" property 
+// 6. On the Vue instance, create a method called "decrement" that subtracts 1 vote from the "votes" property 
 
 // 7. On the #voteCounter span elements, use v-on directive to call the increment or decrement methods
 // so that the vote count goes up and down each time the + or - symbols are clicked. 
 
+new Vue({
+    el: '#app',
+    data: {
+        title: 'Vote for me :)',
+        summary: 'Pleeeaaase!!!',
+        thumbnail: '',
+        alt: 'random cat pic',
+        votes: 0
+    },
+    methods: {
+        increment: function() {
+            this.votes = this.votes + 1
+        },
+        decrement: function() {
+            this.votes = this.votes - 1
+        }
 
+    }
+});
 
+function randInt() {
+    return Math.round(Math.random() * 500 - 10)
+}
+for (const img of document.getElementsByTagName('img')) {
+    img.src = `https://placekitten.com/${randInt() + img.width}/${randInt() + img.height}`
+}
